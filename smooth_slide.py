d2b1d677886fdfc127650c412b1058a22fbd4e34
Ulf Renman <ulf.renman@gmail.com>
2010-07-03 14:30:52 +0200

    Some of the functionality implemented.
    Implemented the accelerationpart and retardationpart params in the code.
    Added some validation of the params. Added commandline handlign to be used
    during development.

--- smooth_slide.py
+++ smooth_slide.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
 import math
 
-def build_move_list(steps,distance):
+class ParamError(Exception): pass
+
+def build_move_list(
+            steps, distance,
+            accelerationpart=50,retardationpart=50,
+            cumulative=False):
     """Build a sequence of movements to make a smooth movement.
 
     The resulting list will consist of the individual steps to make a smooth
@@ -19,7 +25,7 @@ def build_move_list(steps,distance):
     @type  steps:    int
     @param steps:    The number of iterations during which the movement shall
                      be made. Or in other words, the lenght of the resulting
-                     dist.
+                     list.
     @type  distance: int
     @param distance: The distance that should be covered by the smooth
                      movment.
@@ -27,9 +33,43 @@ def build_move_list(steps,distance):
     @return:         A list where the lenght of the list is C{steps} and where
                      the sum of all elements in the list is C{distance}.
     """
-    move_list = [1-math.cos(x*math.pi*2/steps) for x in range(steps)]
+    if not 0<=accelerationpart<=100:
+        raise ParamError('Accelerationpart has to be a % value from 0 to 100')
+    if not 0<=retardationpart<=100:
+        raise ParamError('Retardationpart has to be a % value from 0 to 100')
+    if accelerationpart+retardationpart>100:
+        raise ParamError('Parts has to summurize to a value less then 100')
+
+    acc_part = steps*accelerationpart/100
+    ret_part = steps*retardationpart/100
+    mid_part = steps-acc_part-ret_part
+
+    move_list = (
+            [1-math.cos(x*math.pi/acc_part) for x in range(acc_part)] +
+            [2]*mid_part +
+            [1-math.cos(math.pi+x*math.pi/ret_part) for x in range(ret_part)])
+
     factor = distance/sum(move_list)
     move_list = [round(sum(move_list[:1+x])*factor) for x in range(steps)]
     move_list[-1] = distance
     move_list.insert(0,0)
     return [move_list[x+1]-move_list[x] for x in range(steps)]
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('-s','--steps',type='int',default=40)
+    parser.add_option('-d','--distance', type='int',default=700)
+    parser.add_option('-a','--accelerationpart', type='int',default=50)
+    parser.add_option('-r','--retardationpart', type='int',default=50)
+    parser.add_option('-c','--cumulative', action='store_true' ,default=False)
+
+    (options, args) = parser.parse_args()
+
+    for x in build_move_list(
+                options.steps,
+                options.distance,
+                options.accelerationpart,
+                options.retardationpart,
+                options.cumulative):
+        print '*'*int(x)
